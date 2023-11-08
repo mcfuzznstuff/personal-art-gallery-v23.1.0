b6b1c2727998294ffc162ac4b55a2d8a23e14fb8
Stephanie Miranda <smira6891@live.com>
2023-11-08 09:07:30 -0700

    Started creating slide sections for medium types.
    Comments and the start of slide/Gallery sections.

---
+++ public_html/js/customFuncs.js
@@ -0,0 +1,14 @@
+/*This file will contain custom functions and code written by Stephanie Miranda, @mcfuzznstuff on GitHub. Much of it is done so with support from https://www.w3schools.com/howto/
+
+* Author: Stephanie Miranda, @mcfuzznstuff
+* Date: 11/07/2023
+* <version23 className="1 1"></version23>
+*/
+
+class artGallery {
+     static async slideshow(){
+          console.log("Ya did it @mcfuzznstuff!");
+     }
+}
+
+artGallery.slideshow();
